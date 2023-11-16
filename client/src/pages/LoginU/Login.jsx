7999e77e54dce63102ba3b09593bebfa1ce2514d
Sandeep Patel <sanpatel701@gmail.com>
2023-11-17 04:01:04 +0530

    changes

--- client/src/pages/LoginU/Login.jsx
+++ client/src/pages/LoginU/Login.jsx
@@ -9,11 +9,29 @@ function Login() {
   };
   return (
     <div className="container">
-      <h1 className="heading">Log In</h1>
-      <button className="google_btn" onClick={googleAuth}>
-        <img src="./images/google.png" alt="google icon" />
-        <span>Log in with Google</span>
-      </button>
+      <div className="box">
+        <h1 className="heading">Log In</h1>
+        <button className="google_btn" onClick={googleAuth}>
+          <img
+            src="https://www.svgrepo.com/show/475656/google-color.svg"
+            alt="google icon"
+          />
+          <span>Log in with Google</span>
+        </button>
+        <div class="down">
+          <p class="text-xs">
+            By proceeding, you agree to our
+            <a href="/privacy-policy/" class="underline">
+              Terms of Use
+            </a>
+            and confirm you have read our
+            <a href="/privacy-policy/" class="underline">
+              Privacy and Cookie Statement
+            </a>
+            .
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
